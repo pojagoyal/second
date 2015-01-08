8d08ba0bc420ff1c403bf85e2cf04cf8b1252630
pojagoyal <pooja.rani@berylsystems.com>
2015-01-08 13:03:29 +0530

    arthimetic operations

---
+++ arithimetic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "enter the two numbers"
+read a
+read b
+sum=`expr $a + $b`
+mul=`expr $a \* $b`
+subs=`expr $a - $b`
+div=`expr $a / $b`
+c=$b
+
+echo "the sum is : $sum"
+echo "the multiplication of two numbers is : $mul"
+echo "the substraction is : $subs"
+echo "the div is : $div"
+echo "the valye of c is : $c"
+
